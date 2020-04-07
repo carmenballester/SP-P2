facb1a2c9d0604e6c76ffcce55f435b52a7ff105
Carmen <carmenballesterb@gmail.com>
2020-04-07 17:29:30 +0200

    keypoints and features

--- objectDetection.cpp
+++ objectDetection.cpp
@@ -1,7 +1,6 @@
 #include "objectDetection.h"
 
-void spawnViewer(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointCloud)
-{
+void spawnViewer(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointCloud) {
 	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer ("3D Viewer"));
 	viewer->addCoordinateSystem (1.0);
 	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgb(pointCloud);
@@ -14,14 +13,87 @@ void spawnViewer(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointCloud)
 }
 
 //Mostar nube
-void showCloud(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& cloud)
-{
+void showCloud(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& cloud) {
 	// Abrir un visualizador que muestre la nube filtrada
 	boost::thread visualizer = boost::thread(spawnViewer, cloud);
 	cout << "Pulsa para continuar" << endl;
 	cin.get();
 	visualizer.interrupt();
 	visualizer.join();
+}
+
+void keypoints(PointCloud<PointXYZRGBA>::Ptr& cloud, PointCloud<PointXYZRGBA>::Ptr& cloudKP, PointCloud<SHOT352>::Ptr& cloudDes) {
+	// Detector de keypoints
+	UniformSampling<PointXYZRGBA> uniform_sampling;
+	uniform_sampling.setInputCloud(cloud);
+	uniform_sampling.setRadiusSearch(0.05f);
+	uniform_sampling.filter(*cloudKP);
+	// Normales
+	PointCloud<Normal>::Ptr cloudNormal(new PointCloud<Normal> ());
+	NormalEstimationOMP<PointXYZRGBA, Normal> norm_est;
+	norm_est.setKSearch(10);
+	norm_est.setInputCloud(cloud);
+	norm_est.compute (*cloudNormal);
+
+	//Descriptor
+	pcl::SHOTEstimationOMP<PointXYZRGBA, Normal, SHOT352> descr_est;
+	descr_est.setRadiusSearch(0.2f);
+	descr_est.setInputCloud(cloudKP);
+	descr_est.setInputNormals(cloudNormal);
+	descr_est.setSearchSurface(cloud);
+	descr_est.compute(*cloudDes);
+}
+
+void getModel(PointCloud<PointXYZ>::Ptr scene, PointIndices::Ptr& inliers, float threshold) {
+
+	// Definir el objeto que contiene los coeficientes del modelo
+	ModelCoefficients::Ptr coefficients(new ModelCoefficients);
+	// Definir el objeto con el que se va a segmentar (RANSAC)
+	SACSegmentation<pcl::PointXYZ> seg;
+
+	seg.setOptimizeCoefficients (true);
+	// Definir que el modelo que se busca es un plano
+	seg.setModelType (pcl::SACMODEL_PLANE);
+	// Definir que se segmenta con RANSAC
+	seg.setMethodType (pcl::SAC_RANSAC);
+	// Definir el umbral de inliers
+	seg.setDistanceThreshold (threshold);
+	// Definir la nube que se va a segmentar
+	seg.setInputCloud(scene);
+
+	// Llamar al método que segmenta
+	seg.segment(*inliers, *coefficients);
+
+	// Comprobar si se ha encontrado un modelo
+	if (inliers->indices.size () == 0) {
+	    PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+	    exit(-1);
+  	}
+}
+
+void removePlane(PointCloud<PointXYZRGBA>::Ptr& scene, vector<float> threshold) {
+	// Definir los puntos iniciales de la nube
+	int pointsIn = (int)scene->points.size ();
+	// Definir la nube en XYZ a la que le aplicamos RANSAC
+	PointCloud<PointXYZ>::Ptr sceneXYZ(new PointCloud<PointXYZ>);
+	//Definir el objeto para eliminar los puntos
+	ExtractIndices<PointXYZRGBA> extract;
+
+	for(size_t i=0; i<threshold.size(); i++) {
+		// Copiar la escena a una nube de puntos XYZ
+		copyPointCloud(*scene, *sceneXYZ);
+		// Definir el objeto que contiene los inliers del modelo
+		PointIndices::Ptr inliers(new PointIndices);
+
+		// Obtener los inliers del plano
+		getModel(sceneXYZ, inliers, threshold[i]);
+
+		// Extraer de la escena los inliers obtenidos
+		extract.setInputCloud(scene);
+		extract.setIndices(inliers);
+		extract.setNegative(true);
+		extract.filter(*scene);
+	}
 }
 
 void loadImages(PointCloud<PointXYZRGBA>::Ptr& scene, vector<PointCloud<PointXYZRGBA>::Ptr>& objects) {
@@ -47,7 +119,6 @@ void loadImages(PointCloud<PointXYZRGBA>::Ptr& scene, vector<PointCloud<PointXYZ
     glob(folder, filenames);
 
     // Leer cada una de las nubes de puntos y almacenarla en el vector de nubes si se pueden leer correctamente
-
     for(size_t i=0; i<filenames.size(); i++) {
         PointCloud<PointXYZRGBA>::Ptr object (new PointCloud<PointXYZRGBA>);
         if (pcl::io::loadPCDFile<PointXYZRGBA> (filenames[i], *object) == -1) {
@@ -61,23 +132,46 @@ void loadImages(PointCloud<PointXYZRGBA>::Ptr& scene, vector<PointCloud<PointXYZ
 
 int main (int argc, char** argv) {
 
-    PointCloud<PointXYZRGBA>::Ptr scene (new PointCloud<PointXYZRGBA>);
-    vector<PointCloud<PointXYZRGBA>::Ptr> objects;
+	// Definir las nubes para la escena y sus keypoints y el descriptor
+    PointCloud<PointXYZRGBA>::Ptr scene(new PointCloud<PointXYZRGBA>);
+	PointCloud<PointXYZRGBA>::Ptr sceneKP(new PointCloud<PointXYZRGBA>);
+	PointCloud<SHOT352>::Ptr sceneDes(new PointCloud<SHOT352>);
 
+	// Definir los vectores de nubes para los objetos y sus keypoints y los descriptores
+	vector<PointCloud<PointXYZRGBA>::Ptr> objects;
+	vector<PointCloud<PointXYZRGBA>::Ptr> objectsKP;
+	vector<PointCloud<SHOT352>::Ptr> objectsDes;
+	for(size_t i=0; i<4; i++) {
+		PointCloud<PointXYZRGBA>::Ptr object(new PointCloud<PointXYZRGBA>);
+		objectsKP.push_back(object);
+	}
+	for(size_t i=0; i<4; i++) {
+		PointCloud<SHOT352>::Ptr object(new PointCloud<SHOT352>);
+		objectsDes.push_back(object);
+	}
+
+	// Definir los umbrales para eliminar los planos dominantes
+	// Inicializar el vector con el número de iteraciones y el valor del umbral para eliminar los planos dominantes
+	// Se saca por empirismo, tanto los umbrales como el número de iteraciones
+	vector<float> threshold;
+	threshold.push_back(0.08);
+	threshold.push_back(0.04);
+	threshold.push_back(0.02);
+
+	// Cargar la escena_________________________________________________________
     loadImages(scene, objects);
-    // showCloud(scene);
-    // showCloud(objects[0]);
 
-    // Eliminar planos dominantes
+    // Eliminar planos dominantes_______________________________________________
+	removePlane(scene, threshold);
 
-    // Para la escena y cada uno de los objetos
-
-
-        // Calcular keypoints
-
-        // Calcular descriptores
-
-
+    // Extraer los detectores y descriptores____________________________________
+	cout << "escena" << endl;
+	keypoints(scene, sceneKP, sceneDes);
+	for(int i=0; i<4; i++) {
+		cout << "objeto " << i << endl;
+		keypoints(objects[i],objectsKP[i],objectsDes[i]);
+	}
+	
     // Matching entre objetos y escena
 
     // Corregir malos emparejamientos
--- objectDetection.h
+++ objectDetection.h
@@ -1,21 +1,27 @@
 // Carmen Ballester Bernabeu - 54204928T
 // Sheila Sánchez Rodríguez - 49250479G
 
-#include <opencv2/opencv.hpp>
 #include <iostream>
-
-#include <pcl/io/io.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/visualization/cloud_viewer.h>
+#include <opencv2/opencv.hpp>
 
 #include <pcl/common/transforms.h>
-#include <pcl/point_cloud.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/filters/extract_indices.h>
 #include <pcl/filters/filter.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/filters/uniform_sampling.h>
+#include <pcl/features/shot_omp.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/visualization/cloud_viewer.h>
+// #include <pcl/keypoints/sift_keypoint.h>
 
 #include <boost/thread/thread.hpp>
-
 
 using namespace std;
 using namespace cv;
