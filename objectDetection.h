c1654ba1146005403339bc1dd65cfe608fdcad67
Carmen <carmenballesterb@gmail.com>
2020-05-02 12:15:16 +0200

    hartas de esta practica de mierda ni se lo que hemos cambiado ya

--- objectDetection.h
+++ objectDetection.h
@@ -11,15 +11,19 @@
 #include <pcl/features/normal_3d_omp.h>
 #include <pcl/filters/uniform_sampling.h>
 #include <pcl/features/shot_omp.h>
+#include <pcl/keypoints/harris_3d.h>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/registration/correspondence_rejection_distance.h>
+#include <pcl/registration/transformation_estimation.h>
+#include <pcl/registration/transformation_estimation_point_to_plane_lls_weighted.h>
+#include <pcl/registration/transformation_estimation_svd.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/visualization/cloud_viewer.h>
-// #include <pcl/keypoints/sift_keypoint.h>
 
 #include <boost/thread/thread.hpp>
 
