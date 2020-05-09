a1d3f441e922655d5f154949c892fa88bfeba54b
Carmen <carmenballesterb@gmail.com>
2020-05-09 17:37:00 +0200

    transformacion e icp pero sin que funcionen

--- objectDetection.cpp
+++ objectDetection.cpp
@@ -1,8 +1,16 @@
 #include "objectDetection.h"
 
-void spawnViewer(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointCloud) {
+void spawnViewer(PointCloud<PointXYZRGBA>::Ptr pointCloud/*, PointCloud<PointXYZRGBA>::Ptr cloudKP*/) {
 	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer ("3D Viewer"));
-	viewer->addCoordinateSystem (1.0);
+	// viewer->addCoordinateSystem (1.0);
+
+	// Visualizar keypoints
+	// for(size_t i=0; i<cloudKP->size(); i++){
+	// 	string str = boost::lexical_cast<string>(i);
+	// 	viewer->addSphere (cloudKP->points[i], 0.005, 0, 1, 0, str);
+	// }
+
+
 	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgb(pointCloud);
     viewer->addPointCloud<pcl::PointXYZRGBA> (pointCloud, rgb, "id0");
     while(!viewer->wasStopped ()) {
@@ -12,50 +20,133 @@ void spawnViewer(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pointCloud) {
 	viewer->close();
 }
 
-//Mostar nube
-void showCloud(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr& cloud) {
+//Mostar nube y los keypoints
+void showCloud(PointCloud<PointXYZRGBA>::Ptr& cloud/*, PointCloud<PointXYZRGBA>::Ptr& cloudKP*/) {
 	// Abrir un visualizador que muestre la nube filtrada
-	boost::thread visualizer = boost::thread(spawnViewer, cloud);
-	cout << "Pulsa para continuar" << endl;
+	boost::thread visualizer = boost::thread(spawnViewer, cloud/*, cloudKP*/);
+	// cout << "Pulsa para continuar" << endl;
 	cin.get();
 	visualizer.interrupt();
 	visualizer.join();
 }
 
-void transformation(CorrespondencesPtr correspondenceFilt, PointCloud<PointXYZRGBA>::Ptr objectKP, PointCloud<PointXYZRGBA>::Ptr sceneKP) {
-	Eigen::Matrix<double, 4, 4> transform;
+// Mostrar correspondencias
+void spawnViewerCorr(PointCloud<PointXYZRGBA>::Ptr scene, PointCloud<PointXYZRGBA>::Ptr object, CorrespondencesPtr correspondences, PointCloud<PointXYZRGBA>::Ptr sceneKP,  PointCloud<PointXYZRGBA>::Ptr objectKP) {
+	boost::shared_ptr<visualization::PCLVisualizer> viewer(new visualization::PCLVisualizer ("3D Viewer"));
 
-	registration::TransformationEstimationSVD<PointXYZRGBA, PointXYZRGBA, double> trans_est;
-	trans_est.estimateRigidTransformation(sceneKP,correspondenceFilt, objectKP, transform);
+	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgbS(scene);
+	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgbO(object);
+    viewer->addPointCloud<pcl::PointXYZRGBA> (scene, rgbS, "id0");
+	viewer->addPointCloud<pcl::PointXYZRGBA> (object, rgbO, "id1");
+	// Visualizar keypoints de la escena
+	for(size_t i=0; i<sceneKP->size(); i++){
+		string str = boost::lexical_cast<string>(i);
+		viewer->addSphere (sceneKP->points[i], 0.005, 0, 1, 0, str);
+	}
 
+	// Visualizar keypoints del objeto
+	for(size_t i=0; i<objectKP->size(); i++){
+		string str = boost::lexical_cast<string>(i) + "a";
+		viewer->addSphere (objectKP->points[i], 0.005, 0, 1, 0, str);
+	}
 
-
-
-	// registration::TransformationEstimationPointToPlaneLLSWeighted<Normal,Normal,double> te;
-
-	// // Definir la nube en XYZ a la que le aplicamos RANSAC
-	// PointCloud<PointXYZ>::Ptr sceneXYZ(new PointCloud<PointXYZ>);
-	// // Copiar la escena a una nube de puntos XYZ
-	// copyPointCloud(*scene, *sceneXYZ);
-	//
-	// // Definir la nube en XYZ a la que le aplicamos RANSAC
-	// PointCloud<PointXYZ>::Ptr objectXYZ(new PointCloud<PointXYZ>);
-	// // Copiar la escena a una nube de puntos XYZ
-	// copyPointCloud(*object, *objectXYZ);
-
-
-
-	// te.estimateRigidTransformation(*scene, *object,*correspondenceFilt, transform);
+	// Visualizar correspondencias
+	viewer->addCorrespondences<PointXYZRGBA> (objectKP, sceneKP, *correspondences);
+    while(!viewer->wasStopped ()) {
+		viewer->spinOnce(100);
+		boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+	}
+	viewer->close();
 }
 
-void rejection(CorrespondencesPtr correspondence, CorrespondencesPtr correspondenceFilt, PointCloud<PointXYZRGBA>::Ptr& object, PointCloud<PointXYZRGBA>::Ptr& scene) {
+// Mostrar correspondencias
+void showCorrespondences(PointCloud<PointXYZRGBA>::Ptr& scene, PointCloud<PointXYZRGBA>::Ptr& object, CorrespondencesPtr& correspondences,  PointCloud<PointXYZRGBA>::Ptr& sceneKP,  PointCloud<PointXYZRGBA>::Ptr& objectKP) {
+	// Abrir un visualizador que muestre la nube filtrada
+	boost::thread visualizer = boost::thread(spawnViewerCorr, scene, object, correspondences, sceneKP, objectKP);
+	// cout << "Pulsa para continuar" << endl;
+	cin.get();
+	visualizer.interrupt();
+	visualizer.join();
+}
+
+// Mostrar transformacion
+void spawnViewerTrans(PointCloud<PointXYZRGBA>::Ptr scene, PointCloud<PointXYZRGBA>::Ptr object) {
+	boost::shared_ptr<visualization::PCLVisualizer> viewer(new visualization::PCLVisualizer ("3D Viewer"));
+
+	pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgbS(scene);
+	pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZRGBA> colorHandler(object, 255, 0, 0);
+
+    viewer->addPointCloud<pcl::PointXYZRGBA> (scene, rgbS, "id0");
+	viewer->addPointCloud<pcl::PointXYZRGBA> (object, colorHandler, "id1");
+
+	viewer->addCoordinateSystem (1.0);
+	while(!viewer->wasStopped ()) {
+		viewer->spinOnce(100);
+		boost::this_thread::sleep(boost::posix_time::microseconds(100000));
+	}
+	viewer->close();
+}
+
+// Mostrar transformacion
+void showTransformation(PointCloud<PointXYZRGBA>::Ptr& scene, PointCloud<PointXYZRGBA>::Ptr& object) {
+	// Abrir un visualizador que muestre la nube filtrada
+	boost::thread visualizer = boost::thread(spawnViewerTrans, scene, object);
+	// cout << "Pulsa para continuar" << endl;
+	cin.get();
+	visualizer.interrupt();
+	visualizer.join();
+}
+
+void transformation(CorrespondencesPtr correspondenceFilt, PointCloud<PointXYZRGBA>::Ptr object, PointCloud<PointXYZRGBA>::Ptr scene) {
+
+	registration::TransformationEstimationSVD<PointXYZRGBA,PointXYZRGBA>::Matrix4 transform;
+	registration::TransformationEstimationSVD<PointXYZRGBA, PointXYZRGBA> te;
+
+	te.estimateRigidTransformation(*object, *scene, *correspondenceFilt, transform);
+
+	// std::cout << "The Estimated Rotation and translation matrices (using getTransformation function) are : \n" << std::endl;
+	// printf ("\n");
+	// printf ("    | %6.3f %6.3f %6.3f | \n", transform (0,0), transform (0,1), transform (0,2));
+	// printf ("R = | %6.3f %6.3f %6.3f | \n", transform (1,0), transform (1,1), transform (1,2));
+	// printf ("    | %6.3f %6.3f %6.3f | \n", transform (2,0), transform (2,1), transform (2,2));
+	// printf ("\n");
+	// printf ("t = < %0.3f, %0.3f, %0.3f >\n", transform (0,3), transform (1,3), transform (2,3));
+
+	pcl::transformPointCloud(*object, *object, transform);
+	showTransformation(scene, object);
+
+//______________________________________________________________________________
+	// IterativeClosestPoint<PointXYZRGBA, PointXYZRGBA>::Matrix4 transform2;
+	IterativeClosestPoint<PointXYZRGBA, PointXYZRGBA> icp;
+	PointCloud<PointXYZRGBA>::Ptr object_registered;
+
+	icp.setInputSource(object);
+	icp.setInputTarget(scene);
+
+	// Set the max correspondence distance to 5cm (e.g., correspondences with higher distances will be ignored)
+	icp.setMaxCorrespondenceDistance (0.05);
+	// Set the maximum number of iterations (criterion 1)
+	icp.setMaximumIterations (50);
+	// Set the transformation epsilon (criterion 2)
+	icp.setTransformationEpsilon (1e-8);
+	// Set the euclidean distance difference epsilon (criterion 3)
+	icp.setEuclideanFitnessEpsilon (1);
+
+
+	Eigen::Matrix4f transformation = icp.getFinalTransformation ();
+	pcl::transformPointCloud(*object, *object, transformation);
+	showTransformation(scene, object);
+
+}
+
+void rejection(CorrespondencesPtr correspondence, CorrespondencesPtr correspondenceFilt, PointCloud<PointXYZRGBA>::Ptr& object, PointCloud<PointXYZRGBA>::Ptr& scene, float maximumDistance) {
 
 	registration::CorrespondenceRejectorDistance rejector;
 
-	rejector.setInputSource<PointXYZRGBA>(scene);
-	rejector.setInputTarget<PointXYZRGBA>(object);
+	rejector.setInputSource<PointXYZRGBA>(object);
+	rejector.setInputTarget<PointXYZRGBA>(scene);
 	rejector.setInputCorrespondences(correspondence);
-	rejector.setMaximumDistance(5.0);
+	rejector.setMaximumDistance(maximumDistance);
 	rejector.getCorrespondences(*correspondenceFilt);
 
 	cout << "Correspondences after filter: " <<correspondenceFilt->size () << endl;
@@ -63,23 +154,30 @@ void rejection(CorrespondencesPtr correspondence, CorrespondencesPtr corresponde
 
 void matching(CorrespondencesPtr correspondence, PointCloud<SHOT352>::Ptr& objectDes, PointCloud<SHOT352>::Ptr& sceneDes) { //kdTree
 
-  KdTreeFLANN<SHOT352> match_search;
-  match_search.setInputCloud(objectDes);
+	//KDTREE
+	// KdTreeFLANN<SHOT352> match_search;
+	// match_search.setInputCloud(objectDes);
+	//
+	// //  For each scene keypoint descriptor, find nearest neighbor into the model keypoints descriptor cloud and add it to the correspondences vector.
+	// for (size_t i=0; i<sceneDes->size(); i++) { // Si no funciona cambia los ++ de sitio
+	//   vector<int> neigh_indices (1);
+	//   vector<float> neigh_sqr_dists (1);
+	//   if (!isfinite(sceneDes->at(i).descriptor[0])) { //skipping NaNs
+	//     continue;
+	//   }
+	//   int found_neighs = match_search.nearestKSearch(sceneDes->at(i), 1, neigh_indices, neigh_sqr_dists);
+	//   if(found_neighs == 1 && neigh_sqr_dists[0] < 0.7f) //  add match only if the squared descriptor distance is less than 0.25 (SHOT descriptor distances are between 0 and 1 by design)
+	//   {
+	//     Correspondence corr (neigh_indices[0], static_cast<int> (i), neigh_sqr_dists[0]);
+	//     correspondence->push_back (corr);
+	//   }
+	// }
 
-  //  For each scene keypoint descriptor, find nearest neighbor into the model keypoints descriptor cloud and add it to the correspondences vector.
-  for (size_t i=0; i<sceneDes->size(); i++) { // Si no funciona cambia los ++ de sitio
-    vector<int> neigh_indices (1);
-    vector<float> neigh_sqr_dists (1);
-    if (!isfinite(sceneDes->at(i).descriptor[0])) { //skipping NaNs
-      continue;
-    }
-    int found_neighs = match_search.nearestKSearch(sceneDes->at(i), 1, neigh_indices, neigh_sqr_dists);
-    if(found_neighs == 1 && neigh_sqr_dists[0] < 0.7f) //  add match only if the squared descriptor distance is less than 0.25 (SHOT descriptor distances are between 0 and 1 by design)
-    {
-      Correspondence corr (neigh_indices[0], static_cast<int> (i), neigh_sqr_dists[0]);
-      correspondence->push_back (corr);
-    }
-  }
+	//CORRESPONDENCE ESTIMATION
+	registration::CorrespondenceEstimation<SHOT352, SHOT352> est;
+	est.setInputSource(objectDes);
+	est.setInputTarget(sceneDes);
+	est.determineCorrespondences (*correspondence, 0.8f);
 }
 
 void features(PointCloud<PointXYZRGBA>::Ptr& cloud, PointCloud<PointXYZRGBA>::Ptr& cloudKP, PointCloud<SHOT352>::Ptr& cloudDes) {
@@ -91,32 +189,69 @@ void features(PointCloud<PointXYZRGBA>::Ptr& cloud, PointCloud<PointXYZRGBA>::Pt
 	norm_est.setInputCloud(cloud);
 	norm_est.compute(*cloudNormal);
 
-	// SHOT
-	pcl::SHOTEstimationOMP<PointXYZRGBA, Normal, SHOT352> descr_est;
-	descr_est.setRadiusSearch(0.05f);
-	descr_est.setInputCloud(cloudKP);
-	descr_est.setInputNormals(cloudNormal);
-	descr_est.setSearchSurface(cloud);
-	descr_est.compute(*cloudDes);
+	//SHOT
+	SHOTEstimationOMP<PointXYZRGBA, Normal, SHOT352> shot;
 
-	// cout << "Described: " << cloudDes->size() << endl;
+	shot.setRadiusSearch(0.05f);
+	shot.setInputCloud(cloudKP);
+	shot.setInputNormals(cloudNormal);
+	shot.setSearchSurface(cloud);
+	shot.compute(*cloudDes);
+
+	// PFH
+	// PointCloud<Normal>::Ptr cloudNormal(new PointCloud<Normal>);
+	// NormalEstimation<PointXYZRGBA, Normal> normalEstimation;
+	// PFHEstimation<PointXYZRGBA, Normal, PFHSignature125> pfh;
+	// search::KdTree<PointXYZRGBA>::Ptr kdtree(new search::KdTree<PointXYZRGBA>);
+	//
+	// normalEstimation.setInputCloud(cloud);
+	// normalEstimation.setRadiusSearch(0.03);
+	// normalEstimation.setSearchMethod(kdtree);
+	// normalEstimation.compute(*cloudNormal);
+	//
+	// pfh.setInputCloud(cloud);
+	// pfh.setInputNormals(cloudNormal);
+	// pfh.setSearchMethod(kdtree);
+	// pfh.setRadiusSearch(0.05);
+	// pfh.compute(*cloudDes);
+
+	// Display
+	cout << "Described: " << cloudDes->size() << endl;
 }
 
 void keypoints(PointCloud<PointXYZRGBA>::Ptr& cloud, PointCloud<PointXYZRGBA>::Ptr& cloudKP) { // Cambiar el tipo de cloudKP en función del método que se use
-	// UniformSampling
+	// Uniform Sampling
 	UniformSampling<PointXYZRGBA> uniform_sampling;
+
 	uniform_sampling.setInputCloud(cloud);
 	uniform_sampling.setRadiusSearch(0.05f);
 	uniform_sampling.filter(*cloudKP);
 
-	// Harris
-	// HarrisKeypoint3D <PointXYZRGBA, PointXYZI> harris_3d;
-	// harris_3d.setNonMaxSupression(true);
- 	// harris_3d.setInputCloud(cloud);
- 	// harris_3d.setThreshold(0.000001);
- 	// harris_3d.compute(*cloudKP);
+	// ISS
+	// ISSKeypoint3D<PointXYZRGBA, PointXYZRGBA> iss;
+	// search::KdTree<PointXYZRGBA>::Ptr kdtree(new search::KdTree<PointXYZRGBA>);
 	//
-	// cout << "Detected: " << cloudKP->size() << endl;
+	// iss.setInputCloud(cloud);
+	// iss.setSearchMethod(kdtree);
+	// iss.setSalientRadius(6 * 0.005);
+	// iss.setNonMaxRadius(4 * 0.005);
+	// iss.setMinNeighbors(5);
+	// iss.setThreshold21(0.975);
+	// iss.setThreshold32(0.975);
+	// iss.setNumberOfThreads(1);
+	// iss.compute(*cloudKP);
+
+	// Harris
+	// HarrisKeypoint3D <PointXYZRGBA, PointXYZI> harris;
+	//
+  	// harris.setNonMaxSupression (true);
+  	// harris.setInputCloud (cloud);
+  	// harris.setThreshold (1e-6);
+  	// harris.compute (*cloudKP);
+
+	// Display
+	// showCloud(cloud, cloudKP);
+	cout << "Detected: " << cloudKP->size() << endl;
 }
 
 void getModel(PointCloud<PointXYZ>::Ptr scene, PointIndices::Ptr& inliers, float threshold) {
@@ -200,9 +335,9 @@ void loadImages(PointCloud<PointXYZRGBA>::Ptr& scene, vector<PointCloud<PointXYZ
             cerr << "Error al cargar la nube de puntos " + pcdFile + "\n" << endl;
             exit(-1);
         }
+		removeNaNFromPointCloud(*object, *object, indices);
         objects.push_back(object);
     }
-
 }
 
 int main (int argc, char** argv) {
@@ -211,7 +346,7 @@ int main (int argc, char** argv) {
 	// Se saca por empirismo, tanto los umbrales como el número de iteraciones
 	vector<float> threshold;
 	threshold.push_back(0.08);
-	threshold.push_back(0.04);
+	threshold.push_back(0.06);
 	threshold.push_back(0.02);
 
 	// Definir las nubes para la escena y los objetos
@@ -219,7 +354,7 @@ int main (int argc, char** argv) {
 	vector<PointCloud<PointXYZRGBA>::Ptr> objects;
 
 	// Definir los vectores de keypoints
-	// Uniform Sampling
+	// Uniform Sampling - ISS
 	PointCloud<PointXYZRGBA>::Ptr sceneKP(new PointCloud<PointXYZRGBA>);
 	vector<PointCloud<PointXYZRGBA>::Ptr> objectsKP;
 	for(size_t i=0; i<4; i++) {
@@ -243,6 +378,13 @@ int main (int argc, char** argv) {
 		PointCloud<SHOT352>::Ptr object(new PointCloud<SHOT352>);
 		objectsDes.push_back(object);
 	}
+	// PFH
+	// PointCloud<PFHSignature125>::Ptr sceneDes(new PointCloud<PFHSignature125>());
+	// vector<PointCloud<PFHSignature125>::Ptr> objectsDes;
+	// for(size_t i=0; i<4; i++) {
+	// 	PointCloud<PFHSignature125>::Ptr object(new PointCloud<PFHSignature125>);
+	// 	objectsDes.push_back(object);
+	// }
 
 	// Normal
 	// PointCloud<Normal>::Ptr sceneDes(new PointCloud<Normal>);
@@ -259,7 +401,12 @@ int main (int argc, char** argv) {
 		correspondences.push_back(correspondence);
 	}
 
-	// Definir el vector de correspondencias filtradas
+	// Definir el vector de correspondencias filtradas y el vector que contiene los umbrales
+	vector<float> maximumDistance;
+	maximumDistance.push_back(1.1925);
+	maximumDistance.push_back(1.21);
+	maximumDistance.push_back(1.25);
+	maximumDistance.push_back(1.2);
 	vector<CorrespondencesPtr> correspondencesFilt;
 	for(size_t i=0; i<4; i++) {
 		CorrespondencesPtr correspondence(new Correspondences);
@@ -282,23 +429,22 @@ int main (int argc, char** argv) {
 	for(int i=0; i<4; i++) {
 		features(objects[i],objectsKP[i],objectsDes[i]);
 	}
-    // Matching entre objetos y escena__________________________________________
+
+    //Matching entre objetos y escena__________________________________________
 	for(int i=0; i<4; i++) {
 		matching(correspondences[i],objectsDes[i],sceneDes);
-		cout << "Correspondences found: " <<correspondences[i]->size () << endl;
+		cout << "Correspondences found: " << correspondences[i]->size () << endl;
 	}
-
 
     // Corregir malos emparejamientos___________________________________________
 	for(int i=0; i<4; i++) {
-		rejection(correspondences[i],correspondencesFilt[i],objects[i],scene);
-	}
-    // Calcular la transformacion
-	for(int i=0; i<4; i++) {
-		transformation(correspondencesFilt[i],objectsKP[i],sceneKP);
+		rejection(correspondences[i],correspondencesFilt[i],objects[i],scene, maximumDistance[i]);
 	}
 
-    // Refinar el resultado con ICP
+    // Calcular la transformacion y refinar el resultado con ___________________
+	for(int i=0; i<4; i++) {
+		transformation(correspondencesFilt[i],objects[i],scene);
+	}
 
   return 0;
 }
--- objectDetection.h
+++ objectDetection.h
@@ -3,30 +3,39 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
-
 #include <pcl/common/transforms.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/pfh.h>
+#include <pcl/features/shot_omp.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/filter.h>
-#include <pcl/features/normal_3d_omp.h>
 #include <pcl/filters/uniform_sampling.h>
-#include <pcl/features/shot_omp.h>
+#include <pcl/keypoints/iss_3d.h>
 #include <pcl/keypoints/harris_3d.h>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/registration/correspondence_estimation.h>
 #include <pcl/registration/correspondence_rejection_distance.h>
+#include <pcl/registration/icp.h>
 #include <pcl/registration/transformation_estimation.h>
-#include <pcl/registration/transformation_estimation_point_to_plane_lls_weighted.h>
+#include <pcl/registration/transformation_estimation_point_to_plane_weighted.h>
 #include <pcl/registration/transformation_estimation_svd.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/visualization/cloud_viewer.h>
-
+#include <pcl/visualization/pcl_visualizer.h>
 #include <boost/thread/thread.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <pcl/correspondence.h>
+#include <pcl/registration/correspondence_types.h>
 
 using namespace std;
 using namespace cv;
 using namespace pcl;
+
+// Be
